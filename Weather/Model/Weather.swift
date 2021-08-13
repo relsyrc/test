00fa7c92d5c59d8ec2d661165b49003889123d7f
relsyrc <ilagorskov@MacBook-Pro-Ila.local>
2021-08-13 11:40:27 +0300

    Weather refactor conditionString

--- Weather/Model/Weather.swift
+++ Weather/Model/Weather.swift
@@ -22,29 +22,27 @@ struct Weather {
     var tempMin: Int  = 0
     var tempMax: Int = 0
     
-    
     var conditionString: String {
         switch condition {
         case "clear": return "Ясно"
-        case "partly-cloudy": return "малооблачно"
-        case "cloudy": return "облачно с прояснениями"
-        case "overcast": return "пасмурно"
-        case "drizzle": return "морось"
-        case "light-rain": return "небольшой дождь"
-        case "rain": return "дождь"
-        case "moderate-rain": return "умеренно сильный дождь"
-        case "heavy-rain": return "сильный дождь"
-        case "continuous-heavy-rain": return "длительный сильный дождь"
-        case "showers": return "ливень"
-        case "wet-snow": return "дождь со снегом"
-        case "light-snow": return "небольшой снег"
-        case "snow": return "снег"
-        case "snow-showers": return "снегопад"
-        case "hail": return "град"
-        case "thunderstorm": return "гроза"
-        case "thunderstorm-with-rain": return "дождь с грозой"
-        case "thunderstorm-with-hail": return "гроза с градом"
-            
+        case "partly-cloudy": return "Малооблачно"
+        case "cloudy": return "Облачно с прояснениями"
+        case "overcast": return "Пасмурно"
+        case "drizzle": return "Морось"
+        case "light-rain": return "Небольшой дождь"
+        case "rain": return "Дождь"
+        case "moderate-rain": return "Умеренно сильный дождь"
+        case "heavy-rain": return "Сильный дождь"
+        case "continuous-heavy-rain": return "Длительный сильный дождь"
+        case "showers": return "Ливень"
+        case "wet-snow": return "Дождь со снегом"
+        case "light-snow": return "Небольшой снег"
+        case "snow": return "Снег"
+        case "snow-showers": return "Снегопад"
+        case "hail": return "Град"
+        case "thunderstorm": return "Гроза"
+        case "thunderstorm-with-rain": return "Дождь с грозой"
+        case "thunderstorm-with-hail": return "Гроза с градом"
             
         default: return "Загрузка..."
         }
@@ -60,10 +58,7 @@ struct Weather {
         windSpeed = weatherData.fact.windSpeed
         tempMin = weatherData.forecasts.first!.parts.day.tempMin!
         tempMax = weatherData.forecasts.first!.parts.day.tempMax!
-        
     }
     
-    init() {
-        
-    }
+    init() { }
 }
