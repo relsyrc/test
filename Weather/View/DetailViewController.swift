3287987b9be32a47eb789a4d18ad694e676b3fcc
relsyrc <ilagorskov@MacBook-Pro-Ila.local>
2021-08-13 11:37:48 +0300

    DVC install in private

--- Weather/View/DetailViewController.swift
+++ Weather/View/DetailViewController.swift
@@ -10,39 +10,30 @@ import SwiftSVG
 
 class DetailViewController: UIViewController {
     
-    @IBOutlet weak var nameCityLabel: UILabel!
-    @IBOutlet weak var viewCity: UIView!
-    @IBOutlet weak var conditionLabel: UILabel!
-    @IBOutlet weak var maxTempLabel: UILabel!
-    @IBOutlet weak var minTempLabel: UILabel!
-    @IBOutlet weak var windSpeedLabel: UILabel!
-    @IBOutlet weak var pressureLabel: UILabel!
-    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet private weak var nameCityLabel: UILabel!
+    @IBOutlet private weak var viewCity: UIView!
+    @IBOutlet private weak var conditionLabel: UILabel!
+    @IBOutlet private weak var maxTempLabel: UILabel!
+    @IBOutlet private weak var minTempLabel: UILabel!
+    @IBOutlet private weak var windSpeedLabel: UILabel!
+    @IBOutlet private weak var pressureLabel: UILabel!
+    @IBOutlet private weak var tempLabel: UILabel!
     
     var weatherModel: Weather?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         refreshLabels()
-
-       
     }
     
-    func refreshLabels() {
+    private func refreshLabels() {
         self.nameCityLabel.text = weatherModel?.name
         
         let url = URL(string: "https://yastatic.net/weather/i/icons/funky/dark/\(weatherModel!.conditionCode).svg")
         
-        print(url)
-        
-        let weatherImage = UIView(SVGURL: url!) { (image) in
-            image.resizeToFit(self.viewCity.bounds)
-            
-        }
+        let weatherImage = UIView(SVGURL: url!) { (image) in image.resizeToFit(self.viewCity.bounds) }
         
         self.viewCity.addSubview(weatherImage)
-        
         self.conditionLabel.text = weatherModel?.conditionString
         self.maxTempLabel.text = "\(String(describing: weatherModel?.tempMax))"
         self.minTempLabel.text = "\(String(describing: weatherModel?.tempMin))"
@@ -50,6 +41,4 @@ class DetailViewController: UIViewController {
         self.pressureLabel.text = "\(String(describing: weatherModel?.presureMm))"
         self.tempLabel.text = weatherModel?.temperatureString
     }
-    
-
 }
--- Weather/View/ListTableViewController.swift
+++ Weather/View/ListTableViewController.swift
@@ -9,30 +9,28 @@ import UIKit
 
 
 class ListTableViewController: UITableViewController {
-
-    private let emptyCity = Weather()
     
+    private let emptyCity = Weather()
     private var citiesArray = [Weather]()
     private var filterCiryArray = [Weather]()
     private var nameCitiesArray = ["Брест","Витебск", "Гродно", "Гомель", "Минск", "Новосибириск"]
     
     private let searchController = UISearchController(searchResultsController: nil)
-    
     private var searchBarIsEmpty: Bool {
         guard let text = searchController.searchBar.text else {return false}
         return text.isEmpty
     }
-    
     private var isFiltering: Bool {
         return searchController.isActive && !searchBarIsEmpty
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         if citiesArray.isEmpty {
             citiesArray = Array(repeating: emptyCity, count: nameCitiesArray.count)
         }
+        
         addCities()
         
         searchController.searchResultsUpdater = self
@@ -45,13 +43,11 @@ class ListTableViewController: UITableViewController {
     }
     
     @IBAction private func pressPlusButton(_ sender: Any) {
-        
         alertPlusCity(name: "Город", placeholder: "Введите название города") { city in
             self.nameCitiesArray.append(city)
             self.citiesArray.append(self.emptyCity)
             self.addCities()
         }
-        
     }
     
     private func addCities() {
@@ -65,24 +61,22 @@ class ListTableViewController: UITableViewController {
         }
     }
     
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         if isFiltering {
             return filterCiryArray.count
         }
-        
         return citiesArray.count
     }
-
-
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ListTableViewCell
         
         var weather = Weather()
-  
+        
         if isFiltering {
             weather = filterCiryArray[indexPath.row]
         } else {
@@ -90,31 +84,23 @@ class ListTableViewController: UITableViewController {
         }
         
         cell.configure(weather: weather)
-
-       
-
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        
-            
         let deleteAction = UIContextualAction(style: .destructive, title: "Удалить") { _, _, completionHandler in
-            
             let editingRow = self.nameCitiesArray[indexPath.row]
             
             if let index = self.nameCitiesArray.firstIndex(of: editingRow) {
-                
                 if self.isFiltering {
                     self.filterCiryArray.remove(at: index)
                 } else {
                     self.citiesArray.remove(at: index)
                 }
             }
-            
             tableView.reloadData()
         }
-        
         return UISwipeActionsConfiguration(actions: [deleteAction])
     }
     
@@ -126,12 +112,10 @@ class ListTableViewController: UITableViewController {
                 let filter = filterCiryArray[indexPath.row]
                 let dstVC = segue.destination as! DetailViewController
                 dstVC.weatherModel = filter
-           
             } else {
-            
-            let cityWeather = citiesArray[indexPath.row]
-            let dstVC = segue.destination as! DetailViewController
-            dstVC.weatherModel = cityWeather
+                let cityWeather = citiesArray[indexPath.row]
+                let dstVC = segue.destination as! DetailViewController
+                dstVC.weatherModel = cityWeather
             }
         }
     }
